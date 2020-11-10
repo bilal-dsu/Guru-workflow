3f6a7af73b811a0dfe26e5849b4e55dcf108d941
Sufyan Faizi <47991440+sufyanfaizi@users.noreply.github.com>
2020-11-10 18:45:12 +0500

    Update 10.AuthorCitCentr.py

--- 10.AuthorCitCentr.py
+++ 10.AuthorCitCentr.py
@@ -1,3 +1,11 @@
+# Authors Bilal Hayat Butt, Sufyan Faizi
+# Description The code is used to apply centrality analysis on author citation network
+# First, author citation graph is loaded in memory
+# Second, SNAP and Netowkrx python API's were used to calculate the scores of below centrality measures
+# Indegree and Out degree centrality
+# Eigen centrality
+# Betweness centrality
+
 import snap ,  networkx as nx
 import os, sys
 
